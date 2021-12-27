36d443383e95a5e354cf9a971ddf6f4279886e48
ABHISHEK NAYAK <88398586+ABHISHEKnayak0110@users.noreply.github.com>
2021-12-27 21:07:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 import './App.css';
 import About from "./Component/About/About";
